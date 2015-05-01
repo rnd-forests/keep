1e6dc5f9c3672c63f6dc840b50ea99b14fbf79e3
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-05-01 22:24:27 +0700

    Get tasks associated with a priority level.

--- app/Http/Controllers/PrioritiesController.php
+++ app/Http/Controllers/PrioritiesController.php
@@ -1,5 +1,36 @@
 <?php namespace Keep\Http\Controllers;
 
+use Keep\Repositories\Priority\PriorityRepositoryInterface;
+
 class PrioritiesController extends Controller {
+
+    protected $priorityRepo;
+
+    /**
+     * Create new priorities controller instance.
+     *
+     * @param PriorityRepositoryInterface $priorityRepo
+     */
+    public function __construct(PriorityRepositoryInterface $priorityRepo)
+    {
+        $this->priorityRepo = $priorityRepo;
+    }
+
+    /**
+     * Show all tasks of a user that associated with a priority level.
+     *
+     * @param $userSlug
+     * @param $priorityName
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($userSlug, $priorityName)
+    {
+        $priority = $this->priorityRepo->findByName($priorityName);
+
+        $tasks = $this->priorityRepo->getTasksOfUserAssociatedWithAPriority($userSlug, $priorityName, 10);
+
+        return view('priorities.show', compact('priority', 'tasks'));
+    }
 
 }
---
+++ resources/views/priorities/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title')
+    {{ ucfirst($priority->name) }} Tasks
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="list-group">
+                <a class="list-group-item active"><i class="fa fa-level-up"></i> {{ $priority->name }}</a>
+                @foreach($tasks as $task)
+                    <a class="list-group-item" href="{{ route('users.tasks.show', [Auth::user()->slug, $task->slug]) }}">
+                        <span class="badge">
+                            {{ $task->present()->formatTimeForHumans($task->created_at) }}
+                        </span>
+                        {{ $task->title }}
+                    </a>
+                @endforeach
+            </div>
+            <div class="text-center">{!! $tasks->render() !!}</div>
+        </div>
+    </div>
+@stop
