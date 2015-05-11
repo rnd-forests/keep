a0c35b873f13ba810b8e2268561a6da259c59705
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-05-12 02:04:01 +0700

    Adjust application flash messages.

--- resources/views/vendor/flash/message.blade.php
+++ resources/views/vendor/flash/message.blade.php
@@ -2,13 +2,9 @@
     @if (Session::has('flash_notification.overlay'))
         @include('flash::modal', ['modalClass' => 'flash-modal', 'title' => Session::get('flash_notification.title'), 'body' => Session::get('flash_notification.message')])
     @else
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                <div class="alert alert-{{ Session::get('flash_notification.level') }} global-flash-message">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    {{ Session::get('flash_notification.message') }}
-                </div>
-            </div>
+        <div class="alert alert-{{ Session::get('flash_notification.level') }} global-flash-message">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            {{ Session::get('flash_notification.message') }}
         </div>
     @endif
 @endif
