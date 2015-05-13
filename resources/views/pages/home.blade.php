0458677f352e8bc9cfc85e549bbb543d102dae59
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-05-13 23:34:43 +0700

    Update homepage.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Homepage')
 
 @section('banner')
-    <div class="home-cover" xmlns="http://www.w3.org/1999/html">
-        {!! Html::image('img/home-cover.jpg', $alt = 'Keep home cover photo') !!}
+    <div class="home-cover">
+        {!! Html::image('img/home-cover.png', $alt = 'Keep home cover photo') !!}
     </div>
 @stop
 
@@ -13,17 +13,25 @@
         <div class="home-contents">
             <div class="text-center">
                 @if(Auth::check())
-                    <a href="{{ route('users.tasks.create', Auth::user()) }}">
-                        <button class="btn btn-primary btn-lg">Schedule New Task</button>
-                    </a>
-                    <br/>
-                    <a href="{{ route('users.show', Auth::user()) }}">
-                        <button class="btn btn-info btn-lg">View Your Current Profile</button>
-                    </a>
-                    <br/>
-                    <a href="{{ route('users.dashboard', Auth::user()) }}">
-                        <button class="btn btn-danger btn-lg">Browsing Your Tasks &amp; Assignments</button>
-                    </a>
+                    <div class="well">
+                        <div class="btn-group" role="group">
+                            <a href="{{ route('users.tasks.create', Auth::user()) }}">
+                                <button class="btn btn-primary btn-lg">Schedule Task</button>
+                            </a>
+                            <a href="{{ route('users.dashboard', Auth::user()) }}">
+                                <button class="btn btn-danger btn-lg">Dashboard</button>
+                            </a>
+                            <a href="{{ route('users.show', Auth::user()) }}">
+                                <button class="btn btn-info btn-lg">Personal Profile</button>
+                            </a>
+                            <a href="{{ route('users.assignments.index', Auth::user()) }}">
+                                <button class="btn btn-warning btn-lg">Assignments</button>
+                            </a>
+                            <a href="{{ route('users.groups.index', Auth::user()) }}">
+                                <button class="btn btn-success btn-lg">Groups</button>
+                            </a>
+                        </div>
+                    </div>
                 @else
                     <a href="{{ route('register') }}">
                         <button class="btn btn-danger btn-lg">Register</button>
