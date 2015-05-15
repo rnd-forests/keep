86820e798dc7876ed5af9104f4f30363bcf5bb1f
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-05-15 17:33:37 +0700

    Fix notifable interface.

--- app/Notifications/NotifiableInterface.php
+++ app/Notifications/NotifiableInterface.php
@@ -2,12 +2,6 @@
 
 interface NotifiableInterface {
 
-    public function withSubject($subject);
-    public function withBody($body);
-    public function withType($type);
-    public function regarding($object);
-    public function from($sender);
-    public function to($receiver);
-    public function deliver();
+    public function notify();
 
 }
