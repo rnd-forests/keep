5bdf8fba29f2fda61ba20559227e87fe448637b7
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-05-17 13:32:38 +0700

    Refactor profiles seeder.

--- database/seeds/ProfilesTableSeeder.php
+++ database/seeds/ProfilesTableSeeder.php
@@ -4,41 +4,40 @@ use Keep\Profile;
 use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
-class ProfilesTableSeeder extends Seeder
-{
+class ProfilesTableSeeder extends Seeder {
     public function run()
     {
         $faker = Faker::create();
 
         Profile::create([
-            'user_id' => 1,
-            'location' => 'Thai Phuong - Hung Ha - Thai Binh',
-            'bio' => 'I am a very simple person!',
-            'company'    => 'FPT Software',
-            'website'    => 'http://www.vinhnguyen-hust.com',
-            'phone'      => '01649000000',
+            'user_id'          => 1,
+            'location'         => 'Thai Phuong - Hung Ha - Thai Binh',
+            'bio'              => $faker->paragraph(10),
+            'company'          => 'FPT Software',
+            'website'          => 'http://www.vinhnguyen-hust.com',
+            'phone'            => '01649000000',
             'twitter_username' => 'vinhnguyen',
-            'github_username' => 'vinhnguyentb'
+            'github_username'  => 'vinhnguyentb'
         ]);
 
         Profile::create([
-            'user_id' => 2,
-            'location' => 'Thai Phuong - Hung Ha - Thai Binh',
-            'company'    => 'Deloitte',
-            'website'    => 'http://www.hangdt-eof.com',
-            'phone'      => '01697000000',
+            'user_id'          => 2,
+            'location'         => 'Thai Phuong - Hung Ha - Thai Binh',
+            'bio'              => $faker->paragraph(10),
+            'company'          => 'Deloitte',
+            'website'          => 'http://www.hangdt-eof.com',
+            'phone'            => '01697000000',
             'twitter_username' => 'hangdangaof'
         ]);
 
-        for ($i = 3; $i <= 152; $i++)
-        {
+        for ($i = 3; $i <= 152; $i++) {
             Profile::create([
-                'user_id' => $i,
+                'user_id'  => $i,
                 'location' => $faker->address,
-                'bio' => $faker->paragraph(),
-                'company' => $faker->company,
-                'website' => $faker->url,
-                'phone' => $faker->phoneNumber
+                'bio'      => $faker->paragraph(4),
+                'company'  => $faker->company,
+                'website'  => $faker->url,
+                'phone'    => $faker->phoneNumber
             ]);
         }
     }
