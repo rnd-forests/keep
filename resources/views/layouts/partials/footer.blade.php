7407ec32f90288dbfa9f066db8ef081a7b93afe3
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-05-21 01:49:37 +0700

    Update footer links.

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -5,9 +5,13 @@
         <ul class="footer-links text-muted">
             <li><a href="{{ route('users.show', Auth::user()) }}">Profile</a></li>
             <li>&middot;</li>
-            <li><a href="#">Notifications</a></li>
-            <li>&middot;</li>
             <li><a href="{{ route('users.dashboard', Auth::user()) }}">Dashboard</a></li>
+            <li>&middot;</li>
+            <li><a href="{{ route('users.notifications', Auth::user()) }}">Notifications</a></li>
+            <li>&middot;</li>
+            <li><a href="{{ route('users.assignments.index', Auth::user()) }}">Assignments</a></li>
+            <li>&middot;</li>
+            <li><a href="{{ route('users.groups.index', Auth::user()) }}">Groups</a></li>
         </ul>
     @endif
 </footer>
