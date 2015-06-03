845524c8f275585ced6f4a13aeb244d15f591c51
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-06-03 13:07:46 +0700

    Add social avatar column to profiles table.

---
+++ database/migrations/2015_06_03_125622_add_social_avatar_to_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSocialAvatarToProfilesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('profiles', function(Blueprint $table)
+		{
+			$table->text('social_avatar')->nullable()->after('user_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('profiles', function(Blueprint $table)
+		{
+			$table->dropColumn('social_avatar');
+		});
+	}
+
+}
