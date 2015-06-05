60d457031e40f7cb1e1d483f28bc6f44829c5992
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-06-05 12:09:39 +0700

    Add google oauth credentials.

--- config/services.php
+++ config/services.php
@@ -25,19 +25,19 @@ return [
     'github' => [
         'client_id'     => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect'      => 'http://keep.app/oauth/github',
+        'redirect'      => env('GITHUB_CALLBACK_URL'),
     ],
 
-    'twitter' => [
-        'client_id'     => env('TWITTER_CLIENT_ID'),
-        'client_secret' => env('TWITTER_CLIENT_SECRET'),
-        'redirect'      => 'http://keep.app/oauth/twitter',
+    'google' => [
+        'client_id'     => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect'      => env('GOOGLE_CALLBACK_URL'),
     ],
 
     'facebook' => [
         'client_id'     => env('FACEBOOK_APP_ID'),
         'client_secret' => env('FACEBOOK_APP_SECRET'),
-        'redirect'      => 'http://keep.app/oauth/facebook',
+        'redirect'      => env('FACEBOOK_CALLBACK_URL'),
     ],
 
 ];
