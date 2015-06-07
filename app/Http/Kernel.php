7ae5ff4576c442c1ce569f017d3de58c085f697e
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-06-08 03:47:19 +0700

    Remove clockwork middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,8 +16,7 @@ class Kernel extends HttpKernel
         'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
         'Illuminate\Session\Middleware\StartSession',
         'Illuminate\View\Middleware\ShareErrorsFromSession',
-        'Keep\Http\Middleware\VerifyCsrfToken',
-        'Clockwork\Support\Laravel\ClockworkMiddleware',
+        'Keep\Http\Middleware\VerifyCsrfToken'
     ];
 
     /**
