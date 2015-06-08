07d7a9e498185c55ea6694c7fc330462fafeb423
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-06-08 16:36:26 +0700

    Fix typo.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,6 +3,6 @@
 require_once __DIR__ . '/Routes/authentication.php';
 require_once __DIR__ . '/Routes/pages.php';
 require_once __DIR__ . '/Routes/users.php';
-require_once __DIR__ . '/Routes/administrator.php.php';
+require_once __DIR__ . '/Routes/administrator.php';
 
 Route::post('queue/receive', 'QueuesController@receive');
