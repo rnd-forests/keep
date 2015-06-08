bc0d181728af8fd2d100c41fab525918c61fb9b7
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-06-09 00:23:27 +0700

    Change csrf middleware to exclude some routes.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,25 +2,32 @@
 namespace Keep\Http\Middleware;
 
 use Closure;
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
+    protected $excludedPaths = [
+        'queue/receive',
+    ];
+
     /**
      * Handle an incoming request.
      *
      * @param \Illuminate\Http\Request $request
      * @param callable                 $next
      *
-     * @return mixed
-     * @throws \Illuminate\Session\TokenMismatchException
+     * @return \Illuminate\Http\Response
+     * @throws TokenMismatchException
      */
     public function handle($request, Closure $next)
     {
-        if ($request->path() === 'queue/receive') {
-            return $next($request);
+        $regex = '#' . implode('|', $this->excludedPaths) . '#';
+        if ($this->isReading($request) or $this->tokensMatch($request)
+            or preg_match($regex, $request->path())
+        ) {
+            return $this->addCookieToResponse($request, $next($request));
         }
-
-        return parent::handle($request, $next);
+        throw new TokenMismatchException;
     }
 }
