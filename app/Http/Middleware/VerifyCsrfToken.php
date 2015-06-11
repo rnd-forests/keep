1cd92680e6859562f2358817d05ea04593bbfd62
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-06-11 16:56:38 +0700

    Why don't we use the default???

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,33 +1,16 @@
 <?php
 namespace Keep\Http\Middleware;
 
-use Closure;
-use Illuminate\Session\TokenMismatchException;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
-    protected $excludedPaths = [
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array
+     */
+    protected $except = [
         'queue/subscribe',
     ];
-
-    /**
-     * Handle an incoming request.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param callable                 $next
-     *
-     * @return \Illuminate\Http\Response
-     * @throws TokenMismatchException
-     */
-    public function handle($request, Closure $next)
-    {
-        $regex = '#' . implode('|', $this->excludedPaths) . '#';
-        if ($this->isReading($request) or $this->tokensMatch($request)
-            or preg_match($regex, $request->path())
-        ) {
-            return $this->addCookieToResponse($request, $next($request));
-        }
-        throw new TokenMismatchException;
-    }
 }
