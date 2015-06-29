8bb2f67bc2a3c46d576e936357fe42f53e436f62
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-06-29 12:57:19 +0700

    Add model test helpers.

---
+++ tests/ModelTestHelpers.php
@@ -0,0 +1,99 @@
+<?php
+
+trait ModelTestHelpers
+{
+    public function assertBelongsToMany($parent, $child)
+    {
+        $this->assertRelationship($parent, $child, 'belongsToMany');
+    }
+
+    public function assertBelongsTo($parent, $child)
+    {
+        $this->assertRelationship($parent, $child, 'belongsTo');
+    }
+
+    public function assertHasMany($relation, $class)
+    {
+        $this->assertRelationship($relation, $class, 'hasMany');
+    }
+
+    public function assertHasOne($relation, $class)
+    {
+        $this->assertRelationship($relation, $class, 'hasOne');
+    }
+
+    public function assertMorphMany($relation, $class, $morphable)
+    {
+        $this->assertRelationship($relation, $class, 'morphMany');
+    }
+
+    public function assertMorphTo($relation, $class)
+    {
+        $this->assertRelationship($relation, $class, 'morphTo');
+    }
+
+    public function assertMorphToMany($relation, $class)
+    {
+        $this->assertRelationship($relation, $class, 'morphToMany');
+    }
+
+    public function assertMorphedByMany($relation, $class)
+    {
+        $this->assertRelationship($relation, $class, 'morphedByMany');
+    }
+
+    public function assertRespondsTo($method, $class, $message = null)
+    {
+        $message = $message ?: "Expected the '$class' class to have method, '$method'.";
+        $this->assertTrue(
+            method_exists($class, $method),
+            $message
+        );
+    }
+
+    public function assertRelationship($relationship, $class, $type)
+    {
+        $this->assertRespondsTo($relationship, $class);
+        $args = $this->getArgumentsRelationship($relationship, $class, $type);
+        $class = Mockery::mock($class . "[$type]");
+        switch (count($args)) {
+            case 1 :
+                $class->shouldReceive($type)
+                    ->once()
+                    ->with('/' . str_singular($relationship) . '/i');
+                break;
+            case 2 :
+                $class->shouldReceive($type)
+                    ->once()
+                    ->with('/' . str_singular($relationship) . '/i', $args[1]);
+                break;
+            case 3 :
+                $class->shouldReceive($type)
+                    ->once()
+                    ->with('/' . str_singular($relationship) . '/i', $args[1], $args[2]);
+                break;
+            case 4 :
+                $class->shouldReceive($type)
+                    ->once()
+                    ->with('/' . str_singular($relationship) . '/i', $args[1], $args[2], $args[3]);
+                break;
+            default :
+                $class->shouldReceive($type)
+                    ->once();
+                break;
+        }
+        $class->$relationship();
+    }
+
+    public function getArgumentsRelationship($relationship, $class, $type)
+    {
+        $mocked = Mockery::mock($class . "[$type]");
+        $mocked->shouldReceive($type)
+            ->once()
+            ->andReturnUsing(function () {
+                return func_get_args();
+            });
+
+        return $mocked->$relationship();
+    }
+}
