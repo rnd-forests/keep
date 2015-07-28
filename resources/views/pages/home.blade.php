642aa8f119293737d4fa79905fe5659b19ccd575
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-07-28 12:39:46 +0700

    Homepage?

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,20 +1,19 @@
 @extends('layouts.app')
 @section('title', 'Homepage')
-@section('banner')
-    <div class="home-cover">
-        {!! Html::image('img/home-cover.jpg', $alt = 'Keep home cover photo') !!}
-    </div>
-@stop
 @section('content')
     <div class="row">
         <div class="home-contents">
-            <div class="text-center">
+            <div class="home-buttons col-md-6 col-md-offset-3">
                 @unless(auth()->check())
                     <a href="{{ route('auth::register') }}">
                         <button class="btn btn-danger btn-lg">Sign up for a free account</button>
                     </a>
                     <a href="{{ route('auth::login') }}">
                         <button class="btn btn-primary btn-lg">Log in with your account</button>
+                    </a>
+                @else
+                    <a href="{{ route('member::dashboard', $authUser) }}">
+                        <button class="btn btn-success btn-lg">Visit your dashboard</button>
                     </a>
                 @endif
             </div>
