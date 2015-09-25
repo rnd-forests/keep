2089dee97fd2b34ff5348d7f9ac517ee3fd9ccb1
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-09-25 09:53:02 +0700

    Move raw assets to right location.

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
-var elixir = require("laravel-elixir");
+var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
-    mix.sass("application.scss", "public/css/application.css");
-    mix.less("bootstrap/bootstrap.less", "public/css/bootstrap.css");
+    mix.sass('application.scss', 'public/css/application.css');
+    mix.less('bootstrap/bootstrap.less', 'public/css/bootstrap.css');
 
     mix.styles([
-        "vendor/font-awesome.css",
-        "vendor/datetimepicker.css",
-        "bootstrap.css",
-        "vendor/bootstrap-social.css",
-        "vendor/sweet-alert.css",
-        "application.css"
-    ], null, "public/css");
+        'vendor/font-awesome.css',
+        'vendor/datetimepicker.css',
+        'bootstrap.css',
+        'vendor/bootstrap-social.css',
+        'vendor/sweet-alert.css',
+        'application.css'
+    ], null, 'public/css');
 
     mix.scripts([
-        "vendor/jquery.min.js",
-        "vendor/moment.min.js",
-        "vendor/bootstrap.min.js",
-        "vendor/chart.min.js",
-        "vendor/datepicker.min.js",
-        "vendor/select2.min.js",
-        "vendor/sweet-alert.min.js",
-        "application.js"
-    ]);
+        'vendor/jquery.min.js',
+        'vendor/moment.min.js',
+        'vendor/bootstrap.min.js',
+        'vendor/chart.min.js',
+        'vendor/datepicker.min.js',
+        'vendor/select2.min.js',
+        'vendor/sweet-alert.min.js',
+        'app/application.js'
+    ], null, 'public/js');
 
     mix.version([
-        "public/css/all.css",
-        "public/js/all.js"
+        'public/css/all.css',
+        'public/js/all.js'
     ]);
 });
