fe79c95538fe7954d9f4a0c85c27b1dcf6018eb8
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2015-09-28 23:00:33 +0700

    Fix social authentication view.

--- resources/views/auth/partials/_social_auth.blade.php
+++ resources/views/auth/partials/_social_auth.blade.php
@@ -1,20 +1,20 @@
 <div class="modal fade" id="social-auth">
     <div class="modal-dialog modal-sm">
         <div class="modal-content">
-            <a class="btn btn-block btn-social btn-lg btn-google"
+            <a class="btn btn-block btn-social btn-google"
                href="{{ route('oauth::google') }}">
                 <i class="fa fa-google"></i>
-                Sign in with Google
+                Authentication with Google
             </a>
-            <a class="btn btn-block btn-social btn-lg btn-github"
+            <a class="btn btn-block btn-social btn-github"
                href="{{ route('oauth::github') }}">
                 <i class="fa fa-github"></i>
-                Sign in with GitHub
+                Authentication with GitHub
             </a>
-            <a class="btn btn-block btn-social btn-lg btn-facebook"
+            <a class="btn btn-block btn-social btn-facebook"
                href="{{ route('oauth::facebook') }}">
                 <i class="fa fa-facebook"></i>
-                Sign in with Facebook
+                Authentication with Facebook
             </a>
         </div>
     </div>
